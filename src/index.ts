7202f4d4a2167f7a9d0e0c33e5c0ce097f874f49
Stanyslas Bres <stanyslas.bres@gmail.com>
2019-06-06 11:04:11 +0200

    feat(adapter): use a single default export for adapter and registerMessageHandler

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+import { AxiosRequestConfig, AxiosPromise } from 'axios';
 import { adapter } from './adapter';
 import { registerMessageHandler } from './registerMessageHandler';
 
+interface AxiosChromeMessagingAdapter {
+  (config: AxiosRequestConfig): AxiosPromise;
+
+  registerMessageHandler(): void;
+}
+
+(adapter as AxiosChromeMessagingAdapter).registerMessageHandler = registerMessageHandler;
+
 // tslint:disable-next-line:no-default-export
-export default {
-  adapter,
-  registerMessageHandler,
-};
+export default adapter;
