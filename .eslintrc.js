2f04d0dfd5eec0c9f27b3f4b84b79076948ae4ee
dzetah <dzetah@pm.me>
2021-02-26 12:48:49 +0100

    chore: replace tsling with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "root": true,
+    "parser": "@typescript-eslint/parser",
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "extends": [
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "rules": {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/explicit-module-boundary-types": [
+            "error",
+            {
+                "allowArgumentsExplicitlyTypedAsAny": true
+            }
+        ]
+    }
+};
