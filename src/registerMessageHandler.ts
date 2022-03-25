09a0961129305e069825999fc2cda846e0e1ecf8
Arnþór Snær Sævarsson <arnthorsnaer@tempo.io>
2022-03-25 10:03:57 +0000

    Enabling Manifest V3 support - Tested in v2 and v3

--- src/registerMessageHandler.ts
+++ src/registerMessageHandler.ts
@@ -1,3 +1,5 @@
+import 'regenerator-runtime/runtime';
+import fetchAdapter from '@vespaiach/axios-fetch-adapter';
 import axios from 'axios';
 import {
   AxiosMessagingAdapterRequest,
@@ -14,6 +16,27 @@ export function registerMessageHandler(): void {
     ) => {
       if (request.name === 'axiosMessagingAdapterRequest') {
         // perform axios request in the background script
+
+        // If running in a Manifest v3 service worker
+        if (typeof XMLHttpRequest == 'undefined') {
+          request.axiosConfig.adapter = fetchAdapter;
+        }
+
+        // Fixes breaking requests
+        if (
+          request.axiosConfig.transformRequest.length === 1 &&
+          request.axiosConfig.transformRequest[0] === null
+        ) {
+          request.axiosConfig.transformRequest = [];
+        }
+
+        // Fixes breaking requests
+        if (
+          request.axiosConfig.transformResponse.length === 1 &&
+          request.axiosConfig.transformResponse[0] === null
+        ) {
+          request.axiosConfig.transformResponse = [];
+        }
 
         if (isArrayBufferDataUrl(request.axiosConfig.data)) {
           request.axiosConfig.data = dataURLToArrayBuffer(
